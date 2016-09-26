fa4ce930bd6a7f9263a1ec80a9c0095badd77fd4
Peregrin <peregringaret@gmail.com>
2016-09-25 23:35:07 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
 # Add  code here!
+def prime?(num)
+  if num <= 1
+    false
+  elsif num <= 3
+    true
+  elsif num % 2 == 0 || num % 3 == 0
+    false
+  else
+    i = 5
+    while i * i < num
+        if num % i == 0 || num % (i + 2) == 0
+          false
+        end
+        i = i + 6
+    end
+    true 
+  end
+end
